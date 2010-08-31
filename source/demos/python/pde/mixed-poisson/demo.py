a4161f3bb9a99d816f8b3d703ee054c8c9715a44
Marie E. Rognes <meg@simula.no>
2010-08-31 12:07:06 +0200

    Improved mixed-poisson demo. Feel free to read through. (No, I will not change the signs back again.) Will add cpp version now.

--- source/demos/python/pde/mixed-poisson/demo.py
+++ source/demos/python/pde/mixed-poisson/demo.py
@@ -1,17 +1,17 @@
 """This demo program solves the mixed formulation of Poisson's
 equation:
 
-    sigma + grad(u) = 0
+    sigma - grad(u) = 0
          div(sigma) = f
 
 The corresponding weak (variational problem)
 
-    <tau, sigma> - <div(tau), u> = 0       for all tau
-                 <v, div(sigma)> = <w, f>  for all v
+    <sigma, tau> + <div(tau), u>   = 0       for all tau
+                   <div(sigma), v> = <f, v>  for all v
 
-is solved using BDM (Brezzi-Douglas-Marini) elements of degree q (tau,
-sigma) and DG (discontinuous Galerkin) elements of degree q - 1 for
-(w, u).
+is solved using BDM (Brezzi-Douglas-Marini) elements of degree k for
+(sigma, tau) and DG (discontinuous Galerkin) elements of degree k - 1
+for (u, v).
 
 Original implementation: ../cpp/main.cpp by Anders Logg and Marie Rognes
 """
@@ -22,6 +22,7 @@ __copyright__ = "Copyright (C) 2007 Kristian B. Oelgaard"
 __license__   = "GNU LGPL Version 2.1"
 
 # Modified by Marie E. Rognes 2010
+# Last changed: 31-08-2010
 
 # Begin demo
 
@@ -33,37 +34,42 @@ mesh = UnitSquare(32, 32)
 # Define function spaces and mixed (product) space
 BDM = FunctionSpace(mesh, "BDM", 1)
 DG = FunctionSpace(mesh, "DG", 0)
-V = BDM * DG
+W = BDM * DG
 
 # Define trial and test functions
-(sigma, u) = TrialFunctions(V)
-(tau, v) = TestFunctions(V)
+(sigma, u) = TrialFunctions(W)
+(tau, v) = TestFunctions(W)
 
 # Define source function
 f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
 
 # Define variational form
-a = (dot(sigma, tau) - u*div(tau) + div(sigma)*v)*dx
-L = v*f*dx
-
-# Define essential boundary (y = 0 or y = 1)
-def on_boundary(x):
-    return x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS
+a = (dot(sigma, tau) + div(tau)*u + div(sigma)*v)*dx
+L = - f*v*dx
 
 # Define function G such that G \cdot n = g
-class Flux(Expression):
+class BoundarySource(Expression):
     def eval_data(self, values, data):
-        g = - sin(5*data.x()[0])
+        g = sin(5*data.x()[0])
         values[0] = g*data.normal()[0]
         values[1] = g*data.normal()[1]
     def rank(self):
         return 1
     def dim(self):
         return 2
-G = Flux()
-bc = DirichletBC(V.sub(0), G, on_boundary)
+G = BoundarySource()
 
-# Compute solution and plot u
+# Define essential boundary (y = 0 or y = 1)
+def on_boundary(x):
+    return x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS
+
+bc = DirichletBC(W.sub(0), G, on_boundary)
+
+# Compute solution
 problem = VariationalProblem(a, L, bc)
 (sigma, u) = problem.solve().split()
-plot(u, interactive=True)
+
+# Plot sigma and u
+plot(sigma)
+plot(u)
+interactive()
