af4b9ac8f2305e6a1422ece464e9514910d0be17
Garth N. Wells <gnw20@cam.ac.uk>
2010-09-02 12:19:26 +0100

    Refine copy script and sync with demos.

--- utils/copy_demos.sh
+++ utils/copy_demos.sh
@@ -32,8 +32,9 @@ for DEMO_DIR in $DEMO_DIRS; do
     echo
 
     rsync -av \
-        --exclude *.vtu \
-        --exclude *.pvd \
+        --exclude '*~' \
+        --exclude '*.vtu' \
+        --exclude '*.pvd' \
         --exclude CMakeFiles \
         --exclude cmake_install.cmake \
         $DOLFIN_DIR/demo/$DEMO_DIR $DEST_DIR
