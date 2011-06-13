10c03d2d8e9bbea53621465e55f0879348b014aa
Anders Logg <logg@simula.no>
2011-06-13 16:44:29 +0200

    Add line for testing new design by Harish

--- source/conf.py
+++ source/conf.py
@@ -126,6 +126,7 @@ pygments_style = 'sphinx'
 #html_theme = 'classy'
 #html_theme = 'traditional'
 html_theme = 'fenics'
+#html_theme = 'pylons'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
