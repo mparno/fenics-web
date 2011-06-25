3c232acb3bc5defd7c3e0af1c6c8eddcf20dbc2d
Harish Narayanan <hnarayanan@gmail.com>
2011-06-25 19:16:35 +0200

    Simplified footer and some whitespace fixes

--- source/conf.py
+++ source/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'FEniCS'
-copyright = u'2011, The FEniCS Project, http://www.fenicsproject.org/'
+copyright = u'2011, The FEniCS Project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -250,3 +250,4 @@ def setup(app):
 
 # Include todo notes
 todo_include_todos = True
+html_show_sphinx = False
