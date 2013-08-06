f30dcc36ee539baa754701613ec96e0594d9f964
Anders Logg <logg@simula.no>
2013-08-06 09:12:53 +0200

    Add API key

--- source/_static/js/fetch-news.js
+++ source/_static/js/fetch-news.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     $.ajax({
-	url: 'https://www.googleapis.com/plus/v1/people/105550716956576029273/activities/public?key=INSERT_KEY_HERE',
+	url: 'https://www.googleapis.com/plus/v1/people/105550716956576029273/activities/public?key=AIzaSyDmcx1Nx8NOiFL_sSwfgq6ovjfJRl6Kf8g',
 	success: function(news) {
 	    $('#news').append('<p>' + news.items[0].object.content + '</p>');
 	    $('#news').append('<small>' + news.items[0].actor.displayName + '&mdash;' + news.items[0].published + '</small>');
