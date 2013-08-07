822ad333a0206f578d92945a0e72f1675fa34f19
Anders Logg <logg@simula.no>
2013-08-07 10:53:14 +0200

    Add Google+ feed to front page using script from Harish

--- source/_static/js/fetch-news.js
+++ source/_static/js/fetch-news.js
@@ -2,8 +2,18 @@ window.onload = function() {
     $.ajax({
 	url: 'https://www.googleapis.com/plus/v1/people/105550716956576029273/activities/public?key=AIzaSyDmcx1Nx8NOiFL_sSwfgq6ovjfJRl6Kf8g',
 	success: function(news) {
-	    $('#news').append('<p>' + news.items[0].object.content + '</p>');
-	    $('#news').append('<small>' + news.items[0].actor.displayName + '&mdash;' + news.items[0].published + '</small>');
+            for (var i = 0; i < 7; i++)
+            {
+	        $('#news').append('<p>');
+	        $('#news').append(news.items[i].title +
+                                  ' ' +
+                                  '<a href="' + news.items[i].url + '">Read more &raquo;</a>');
+	        $('#news').append('<small><br>' +
+                                  'Posted by <a href="' + news.items[i].actor.url + '">' + news.items[i].actor.displayName + '</a>' +
+                                  '&mdash;' + news.items[i].published +
+                                  '</small>');
+	        $('#news').append('</p>');
+            }
 	},
 	dataType: 'json'
     });
