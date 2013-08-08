493bc444802dc7b14f36df9728506b3cecbbdae5
Harish Narayanan <hnarayanan@gmail.com>
2013-08-08 15:40:59 +0200

    Added poster's thumbnail avatar to recent project news

--- source/_static/js/fetch-news.js
+++ source/_static/js/fetch-news.js
@@ -5,9 +5,10 @@ window.onload = function() {
             for (var i = 0; i < 7; i++)
             {
 	        $('#news').append('<p>');
+		$('#news').append('<a href="' + news.items[i].url + '"><img class="avatar" src="' + news.items[i].actor.image.url + '"></a>');
 	        $('#news').append(news.items[i].title +
                                   ' ' +
-                                  '<a href="' + news.items[i].url + '">Read more &raquo;</a>');
+                                  '<a href="' + news.items[i].url + '"><br>Read more &raquo;</a>');
 	        $('#news').append('<small><br>' +
                                   'Posted by <a href="' + news.items[i].actor.url + '">' + news.items[i].actor.displayName + '</a>' +
                                   '&mdash;' + news.items[i].published +
